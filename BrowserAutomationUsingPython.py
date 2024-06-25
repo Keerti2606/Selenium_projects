9955dc125984e46427ee4794dbee1037a00bb876
HP <kirti266@gmail.com>
2024-06-25 19:01:36 +0530

    second

--- BrowserAutomationUsingPython.py
+++ BrowserAutomationUsingPython.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+
+
 
 chrome_options = Options()
 chrome_options.add_argument("--start-maximized")
@@ -10,3 +14,21 @@ chrome_path = r'C:\\Users\\HP\\Desktop\\gitstuff\\chromedriver.exe'
 driver = webdriver.Chrome(service=Service(chrome_path),options=chrome_options)
 
 driver.get("https://practicetestautomation.com/practice-test-login/")
+
+time.sleep(10)
+
+user = driver.find_element(By.ID,'username')
+user.send_keys('student')
+
+time.sleep(10)
+
+passw = driver.find_element(By.ID,'password')
+
+with open('test.txt','r') as myfile:
+    password = myfile.read().replace('\n',' ')
+passw.send_keys(password)
+
+submit = driver.find_element(By.ID,'submit')
+submit.click()
+
+
